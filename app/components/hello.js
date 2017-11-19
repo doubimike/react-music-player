8cd458ac2cb599ce0b7ee378a553f4f937211d75
zhanghang <mike.zhang@yeamoney.cn>
2017-11-19 22:46:57 +0800

    完成热更新等基本环境的搭建

---
+++ app/components/hello.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './hello.less'
+
+
+let Hello = React.createClass({
+    render(){
+        return (
+                <div className="hello-component">
+                    Hello,React Mikeaa  aaaaaaaaaaa
+                </div>
+            );
+    }
+})
+
+export default Hello;
---
+++ app/index.js
@@ -0,0 +1,29 @@
+import Hello from './components/hello';
+import React from 'react'
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+
+
+render(
+    <AppContainer>
+        <Hello />
+    </AppContainer>,
+    document.getElementById('root')
+);
+
+if (module.hot) {
+    module.hot.accept('./components/hello', () => {
+        const NewHello = require('./components/hello').default;
+        render(
+            <AppContainer>
+                <NewHello />
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
+}
+
+
+
+
